651cb8d6181c329413037e8e232ea29c826dd13e
Gabor Szasz <skywallalp@gmail.com>
2021-11-15 20:28:46 +0100

    promise for test

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,7 @@
-
-
-
-
-
+const converterPromise = (arr = []) => new Promise((resolve, reject) => {
+  const isStrings = arr.every((v) => typeof v === 'string');
+  if (!isStrings) { reject('Error: Not all elements are string type!'); } else { resolve(arr.map((v) => v.toUpperCase())); }
+});
 
 // export default converterPromise;
+export default converterPromise;
